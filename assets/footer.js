d4dd0f4deb009acd84cd122fd24a9841c19685ae
Frank Krüger <fkrueger@mp-group.net>
2023-07-11 16:58:49 +0200

    [TASK] Codeanpassungen

--- assets/footer.js
+++ assets/footer.js
@@ -1,13 +1,7 @@
 'use strict';
 
-// footer.scss
-import './styles/footer.scss';
-
 // main js
 require('./scripts/main');
-
-// vue js
-//require('./scripts/vue');
 
 // bootrstrap table formatter js
 require('./scripts/table/formatter');
